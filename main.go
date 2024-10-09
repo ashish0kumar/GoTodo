8f94123453eb77242140f2998a51f63a0a5891e4
Ashish Kumar <ashishkumar901336@gmail.com>
2024-10-10 00:37:03 +0530

    completed

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 func main() {
 	todos := Todos{}
+
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
 
-	todos.print()
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
 
 	storage.Save(todos)
 }
